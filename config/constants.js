6a7ada0a65d5306887e56cc7280c1a9cf90575e0
Mkhotamirais <mkhotamirais@gmail.com>
2024-03-02 18:39:01 +0700

    masalah keys

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,11 @@
-const { VITE_OMDBAPI_KEY: omdbapiKey } = import.meta.env;
+const keys = {
+  omdbapi: "99773434",
+  newsapi: "9c6131f2202d4dafb822bc76564f80c4",
+};
 
 const url = {
   jsonplaceholderUrl: "https://jsonplaceholder.typicode.com",
-  omdbapiUrl: `http://www.omdbapi.com/?apikey=${omdbapiKey}&`,
+  omdbapiUrl: `http://www.omdbapi.com/?apikey=${keys.omdbapi}&`,
 };
 
 export const { jsonplaceholderUrl: jpUrl, omdbapiUrl: omdbUrl } = url;
